c7fc7ab11414dd4f27ed0c8528c6aec034b53407
Vũ Minh Hùng <74605414+vmhungltt@users.noreply.github.com>
2022-05-04 14:16:38 +0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { StoreProvider } from "./store";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <StoreProvider>
+        <App  />
+      </StoreProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+// mười điểm mười cái lol
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
